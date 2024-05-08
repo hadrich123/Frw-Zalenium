622d6721321a47f04ec88c8d4cc5d9f93fa616e2
hadrich123 <hedrich.ahmed@gmail.com>
2024-05-08 17:02:32 +0100

    first commit

---
+++ src/test/java/testCase/ExecuteTestZaleniumFirefox.java
@@ -0,0 +1,23 @@
+package testCase;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+public class ExecuteTestZaleniumFirefox {
+
+	@Test
+	public void test1() throws MalformedURLException {
+		System.out.println("thread which is running"+ Thread.currentThread().getId());
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setBrowserName(BrowserType.FIREFOX);
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		System.out.println(driver.getTitle()); 
+	}
+}
